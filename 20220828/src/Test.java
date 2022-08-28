88e56e2bc71d71d8fda3dd3fc8fe61e977d22719
cct010 <107844670+cct010@users.noreply.github.com>
2022-08-28 21:18:29 +0800

    Create Test.java

---
+++ 20220828/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-08-28
+ * Time: 10:31
+ */
+public class Test {
+    public static void main(String[] args) {
+        ListA list = new ListA();
+        list.creatList();
+        list.display();
+    }
+}
