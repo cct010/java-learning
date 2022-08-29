79d90774691b99fc6cc67c0b636075d32fd66847
cct010 <107844670+cct010@users.noreply.github.com>
2022-08-29 20:06:53 +0800

    Create Test.java

---
+++ 20220829/src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-08-29
+ * Time: 19:46
+ */
+public class Test {
+    public static void main(String[] args) {
+        MyLinkedList l1 = new MyLinkedList();
+        
+        boolean b = l1.contains(4);
+        System.out.println(b);
+    }
+}
