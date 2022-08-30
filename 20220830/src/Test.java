0269cde7eb762e9731eb7063eeaf018230a2bc36
cct010 <107844670+cct010@users.noreply.github.com>
2022-08-30 20:32:03 +0800

    Create Test.java

---
+++ 20220830/src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-08-30
+ * Time: 19:49
+ */
+public class Test {
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+        l1.createList();
+        l1.dispay();
+        System.out.println(l1.Loop());
+    }
+}
