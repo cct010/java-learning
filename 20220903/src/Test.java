43243c58c63d05d1183b2ee54f6f62a9cd70603b
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-04 20:00:48 +0800

    Create Test.java

---
+++ 20220903/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-09-04
+ * Time: 19:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = "hh";
+        dog.eat();
+        Bird bird = new Bird();
+        bird.name = "ff";
+        bird.eat();
+    }
+}
