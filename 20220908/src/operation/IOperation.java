317a8aefbdd9db4974606fb03602e91e81fc987b
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-08 20:14:30 +0800

    Create IOperation.java

---
+++ 20220908/src/operation/IOperation.java
@@ -0,0 +1,13 @@
+package operation;
+import book.Book;
+import book.BookList;
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-09-08
+ * Time: 19:26
+ */
+public interface IOperation {
+    void work(BookList bookList);
+}
