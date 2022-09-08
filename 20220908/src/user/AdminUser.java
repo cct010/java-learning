947a4b6b978ad0570d2e77869dd6dcb0e3bc1be5
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-08 20:14:34 +0800

    Create AdminUser.java

---
+++ 20220908/src/user/AdminUser.java
@@ -0,0 +1,13 @@
+package user;
+import book.Book;
+import book.BookList;
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-09-08
+ * Time: 19:47
+ */
+public class AdminUser {
+
+}
