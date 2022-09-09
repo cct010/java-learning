185e381ef268c5bc7354639453ba7003a18cbcb9
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-09 21:58:10 +0800

    Update AdminUser.java

--- 20220908/src/user/AdminUser.java
+++ 20220908/src/user/AdminUser.java
@@ -1,6 +1,10 @@
 package user;
 import book.Book;
 import book.BookList;
+import operation.*;
+
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description
@@ -8,6 +12,29 @@ import book.BookList;
  * Date: 2022-09-08
  * Time: 19:47
  */
-public class AdminUser {
+public class AdminUser extends User{
+    public AdminUser(String name) {
+        super(name);
+        this.iOperations = new IOperation[]{
+                new ExitOperation(),
+                new FindOperation(),
+                new AddOperation(),
+                new DelOperation(),
+                new DisplayOperation()
+        };
+    }
 
+    @Override
+    public int menu() {
+        System.out.println("============欢迎"+this.name+"来到图书馆=========");
+        System.out.println("1.查找图书");
+        System.out.println("2.新增图书");
+        System.out.println("3.删除图书");
+        System.out.println("4.显示图书");
+        System.out.println("0.退出系统");
+        System.out.println("=================================");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        return choice;
+    }
 }
