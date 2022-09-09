d0edba74a018c8805de35267501d5d23840e5f1b
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-09 21:58:00 +0800

    Update ExitOperation.java

--- 20220908/src/operation/ExitOperation.java
+++ 20220908/src/operation/ExitOperation.java
@@ -1,5 +1,7 @@
 package operation;
 
+import book.BookList;
+
 /**
  * Created with IntelliJ IDEA.
  * Description
@@ -7,5 +9,9 @@ package operation;
  * Date: 2022-09-08
  * Time: 19:25
  */
-public class ExitOperation {
+public class ExitOperation implements IOperation{
+    @Override
+    public void work(BookList bookList) {
+
+    }
 }
