551d5f7cb5a51d438983051ed7c25ec234f189e4
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-09 21:58:13 +0800

    Update NormalUser.java

--- 20220908/src/user/NormalUser.java
+++ 20220908/src/user/NormalUser.java
@@ -1,5 +1,9 @@
 package user;
 
+import operation.*;
+
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description
@@ -7,5 +11,28 @@ package user;
  * Date: 2022-09-08
  * Time: 19:47
  */
-public class NormalUser {
+public class NormalUser extends User{
+    public NormalUser(String name) {
+        super(name);
+        this.iOperations = new IOperation[]{
+                new ExitOperation(),
+                new FindOperation(),
+                new AddOperation(),
+                new DelOperation(),
+                new DisplayOperation()
+        };
+    }
+
+    @Override
+    public int menu() {
+        System.out.println("============欢迎"+this.name+"来到图书馆=========");
+        System.out.println("1.查找图书");
+        System.out.println("2.借阅图书");
+        System.out.println("3.归还图书");
+        System.out.println("0.退出系统");
+        System.out.println("=================================");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        return choice;
+    }
 }
