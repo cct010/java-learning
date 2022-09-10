f28a7e9e5a5fd17cd716e8be37a51719a01fdea6
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-10 19:56:48 +0800

    Update AddOperation.java

--- 20220908/src/operation/AddOperation.java
+++ 20220908/src/operation/AddOperation.java
@@ -1,5 +1,6 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 /**
@@ -14,5 +15,18 @@ public class AddOperation implements IOperation{
     public void work(BookList bookList) {
         System.out.println("新增图书");
         System.out.println("请输入要新增的图书名称：");
+        String name  = scanner.nextLine();
+        System.out.println("请输入作者名字：");
+        String author = scanner.nextLine();
+        System.out.println("请输入图书类型：");
+        String type = scanner.nextLine();
+        System.out.println("请输入图书价格：");
+        int price = scanner.nextInt();
+
+        Book book = new Book(name,author,price,type);
+        int size = bookList.getUsedSize();
+        bookList.setBook(size,book);
+        bookList.setUsedSize(size+1);
+        System.out.println("新增图书成功！");
     }
 }
