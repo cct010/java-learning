912e9e9d79224174569244fb407070d49590b9cd
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-10 19:56:51 +0800

    Update BorrowOperation.java

--- 20220908/src/operation/BorrowOperation.java
+++ 20220908/src/operation/BorrowOperation.java
@@ -1,5 +1,6 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 /**
@@ -12,6 +13,19 @@ import book.BookList;
 public class BorrowOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
-
+        System.out.println("借阅图书！");
+        System.out.println("请输入要借阅的图书名称：");
+        String name = scanner.nextLine();
+        int size = bookList.getUsedSize();
+        for(int i= 0;i<size ;i++){
+            Book book = bookList.getPos(i);
+            if(name.equals(book.getName())){
+                book.setBorrower(true);
+                System.out.println("借阅成功！");
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("没有你要借阅的书！");
     }
 }
