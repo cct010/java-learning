8895bbfa63e1becc9152706e5f2d82c639fcbb69
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-12 20:11:38 +0800

    Update ExitOperation.java

--- 20220908/src/operation/ExitOperation.java
+++ 20220908/src/operation/ExitOperation.java
@@ -12,6 +12,7 @@ import book.BookList;
 public class ExitOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
-
+        System.out.println("退出系统！");
+        System.exit(0);
     }
 }
