b2623c9aa136a4dadc0c8e50e1820e467fd010dc
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-12 20:11:42 +0800

    Update ReturnOperation.java

--- 20220908/src/operation/ReturnOperation.java
+++ 20220908/src/operation/ReturnOperation.java
@@ -1,5 +1,6 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 /**
@@ -12,6 +13,21 @@ import book.BookList;
 public class ReturnOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
-
+        System.out.println("归还图书");
+        System.out.println("请输入要归还的图书名称：");
+        String name = scanner.nextLine();
+        int size = bookList.getUsedSize();
+        for(int i =0;i<size;i++){
+            Book book = bookList.getPos(i);
+            if(name.equals(book.getName())){
+                if(book.isBorrower()==true){
+                    book.setBorrower(false);
+                    System.out.println("归还成功");
+                    return;
+                }
+                System.out.println("书籍未被借出！");
+            }
+        }
+        System.out.println("归还书籍名称错误！");
     }
 }
