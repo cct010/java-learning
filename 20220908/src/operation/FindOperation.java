6f4d50be99e737487f88629057dea09c1d1a363e
cct010 <107844670+cct010@users.noreply.github.com>
2022-09-12 20:11:40 +0800

    Update FindOperation.java

--- 20220908/src/operation/FindOperation.java
+++ 20220908/src/operation/FindOperation.java
@@ -1,5 +1,6 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
 /**
@@ -12,6 +13,17 @@ import book.BookList;
 public class FindOperation implements IOperation{
     @Override
     public void work(BookList bookList) {
-
+        System.out.println("查询图书！");
+        System.out.println("请输入要查询的图书名字：");
+        String name = scanner.nextLine();
+        int size = bookList.getUsedSize();
+        for(int i=0;i<size;i++){
+            Book book = bookList.getPos(i);
+            if(name.equals(book.getName())){
+                System.out.println(book);
+                return ;
+            }
+        }
+        System.out.println("没有你要查询的图书！");
     }
 }
