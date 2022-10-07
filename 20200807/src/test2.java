2cbd161f3d0a37920200c65c9458e9976f0ed67c
cct010 <107844670+cct010@users.noreply.github.com>
2022-10-07 20:09:59 +0800

    Update test2.java

--- 20200807/src/test2.java
+++ 20200807/src/test2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description
@@ -41,5 +43,20 @@ public class test2 {
         System.out.println(20+10+"hello");//计算器在计算的时候从左往右看，就计算20+10，再拼接“hello”
         System.out.println("hello"+(10+20));
         System.out.println(10+""+20+"hello");
+        String str = "123";
+        int ret = Integer.valueOf(str);
+        System.out.println(ret);
+        int num = 113;
+        String ret1 = String.valueOf(num);
+        System.out.println(ret1);
+        Scanner scanner = new Scanner(System.in);
+        //java本身就不是用来开发控制台程序的。
+        int n =scanner.nextInt();
+        if(n%2 ==0) {
+            System.out.println("偶数");
+        }
+        else {
+            System.out.println("奇数");
+        }
     }
 }
