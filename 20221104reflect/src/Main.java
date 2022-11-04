7948a8af4d1c9c5cb39874f51c1cc778c0240018
cct010 <107844670+cct010@users.noreply.github.com>
2022-11-04 20:01:27 +0800

    Create Main.java

---
+++ 20221104reflect/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.Callable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description
+ * User: Administrator
+ * Date: 2022-11-04
+ * Time: 19:49
+ */
+class Student{
+    private String name = "hh";
+    public int age =18;
+
+    public Student() {
+        System.out.println("我的名字叫？");
+    }
+
+    private Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("我的名字叫 "+name+"我才不告诉你我"+age+"岁！");
+    }
+    private void eat(){
+        System.out.println("你没看见我正在吃饭嘛？");
+    }
+    public void sleep(){
+        System.out.println("好啦！我要睡觉了！");
+    }
+    private void function(String str){
+        System.out.println(str);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> c1 = Class.forName("Student");
+        Class<?> c2 = Student.class;
+        Student student = new Student();
+        Class<?> c3 = student.getClass();
+        System.out.println(c3.equals(c2));
+    }
+}
